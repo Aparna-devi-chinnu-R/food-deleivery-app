f8766b4112489ccf9bae33e8349851cb195b06df
Aparna-devi-chinnu-R <aparnadevi.rajavel@thoughtworks.com>
2022-05-13 20:28:53 +0530

    [Aparna] Refactor . styles.

--- src/components/cart/CartItem.js
+++ src/components/cart/CartItem.js
@@ -1,17 +1,19 @@
-const CartItem = ({item}) => {
-    return (
-        <div style={{borderBottom: "2px solid \t#800000", padding: "10px",borderRadius:"5px"}}>
-            <div style={{fontWeight: "bold",display:"flex",justifyContent:"space-between"}}>
-                <div>{item.name}</div>
-                <div style={{display:"flex",width:"25%",justifyContent:"space-between",marginTop:"5px"}}>
-                    <div style={{border:"1px solid \t#800000",padding:"2px 15px"}}>+</div>
-                    <div style={{border:"1px solid \t#800000",padding:"2px 15px"}}>-</div>
-                </div>
+import styles from './CartItemStyles';
 
+const CartItem = ({item}) => {
+    const classes = styles();
+    return (
+        <div className={classes.cartItem}>
+            <div style={{fontWeight: "bold", display: "flex", justifyContent: "space-between"}}>
+                <div>{item.name}</div>
+                <div className={classes.cartButtonBox}>
+                    <div className={classes.cartButton}>+</div>
+                    <div className={classes.cartButton}>-</div>
+                </div>
             </div>
             <div style={{display: "flex"}}>
                 <div style={{color: "darkred"}}>Rs : {item.price}</div>
-                <div style={{border: "1px solid black", marginLeft: "10px", padding: "0 5px"}}>x{item.count}</div>
+                <div className={classes.count}>x{item.count}</div>
             </div>
         </div>
     )
---
+++ src/components/cart/CartItemStyles.js
@@ -0,0 +1,30 @@
+import {makeStyles} from "@material-ui/core";
+
+export default makeStyles(() => ({
+
+    cartItem: {
+        borderBottom: "2px solid \t#800000",
+        padding: "10px",
+        borderRadius: "5px"
+    },
+
+    cartButtonBox: {
+        display: "flex",
+        width: "25%",
+        justifyContent: "space-between",
+        marginTop: "5px"
+    },
+
+    cartButton: {
+        border: "1px solid \t#800000",
+        padding: "2px 15px"
+    },
+
+    count: {
+        border: "1px solid black",
+        marginLeft: "10px",
+        padding: "0 5px"
+    }
+
+
+}))
