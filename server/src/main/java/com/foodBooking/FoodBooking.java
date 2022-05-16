cbea9e5d235aff5a9416f39791e3cc1653cda9ad
Aparna-devi-chinnu-R <aparnadevi.rajavel@thoughtworks.com>
2022-05-16 20:14:26 +0530

    [Aparna] Add . dependencies.

---
+++ server/src/main/java/com/foodBooking/FoodBooking.java
@@ -0,0 +1,13 @@
+package com.foodBooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodBooking {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodBooking.class, args);
+    }
+
+}
